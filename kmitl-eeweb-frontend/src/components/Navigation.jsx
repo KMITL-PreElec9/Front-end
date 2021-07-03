96ecc0c8704e016f28c87fe91d0b06359eef37e2
Knapoj Chaimanekorn <63010104@kmitl.ac.th>
2021-07-03 12:37:24 +0700

    Initial Update

---
+++ kmitl-eeweb-frontend/src/components/Navigation.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+
+function Navigation(props) {
+return (
+  <div className="Navigation">
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+      <h1 class="logo mr-auto"><a href="/">EE KMITL</a></h1>
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="/">หน้าหลักค่าย</a></li>
+          <li><a href="#about">เกี่ยวกับเรา</a></li>
+          <li><a href="#covid">โควิด-19</a></li>
+          <li><a href="#faq">FAQ</a></li>
+          <li><a href="#contact">ติดต่อเรา</a></li>
+          <li class="drop-down">
+            <a>63010104@kmitl.ac.th</a>
+            <ul>
+              <center>
+                <li><a href="/accounts/userprofile?next=/">ข้อมูลส่วนตัว</a></li>
+              </center>
+              <center>
+                <li><a href="/accounts/email?next=/">เปลี่ยนอีเมล</a></li>
+              </center>
+              <li><a class="btn btn-danger btn-block" style={{width:"70%", color:"white", margin:"auto"}}
+                  href="/accounts/logout?next=/">ออกจากระบบ</a></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  </div>
+);
+}
+
+export default withRouter(Navigation);
